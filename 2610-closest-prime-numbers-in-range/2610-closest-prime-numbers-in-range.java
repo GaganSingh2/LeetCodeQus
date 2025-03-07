4fd5318ce7ecad300be1d056100cf765189485e6
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2025-03-08 04:09:58 +0530

    Time: 2041 ms (5.16%), Space: 55.5 MB (14.68%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -1,5 +1,5 @@
 class Solution {
-    static boolean isPrime(int n) {
+    private static boolean isPrime(int n) {
         if(n == 2) return true;
         if (n <= 1 || n%2 == 0)
             return false;
