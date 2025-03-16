ee8f7176449db2db823494a8f08dadd273f00505
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2025-03-16 23:34:44 +0530

    Time: 0 ms (100%), Space: 41.9 MB (65.08%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,32 +1,56 @@
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         int zeroCnt=0, oneCnt = 0, twoCnt = 0;
+//         for(int i=0; i<nums.length; i++){
+//             if(nums[i]==0){
+//                 zeroCnt++;
+//             }
+//             else if(nums[i]==1){
+//                 oneCnt++;
+//             }else{
+//                 twoCnt++;
+//             }
+//         }
+//         int ArrayIterator = 0;
+//         if(ArrayIterator<nums.length){
+//             while(zeroCnt>=1){
+//                 nums[ArrayIterator] = 0;
+//                 ArrayIterator++;
+//                 zeroCnt--;
+//             }
+//             while(oneCnt>=1){
+//                 nums[ArrayIterator] = 1;
+//                 ArrayIterator++;
+//                 oneCnt--;
+//             }
+//             while(twoCnt>=1){
+//                 nums[ArrayIterator] = 2;
+//                 ArrayIterator++;
+//                 twoCnt--;
+//             }
+//         }
+//     }
+// }
+
 class Solution {
     public void sortColors(int[] nums) {
-        int zeroCnt=0, oneCnt = 0, twoCnt = 0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]==0){
-                zeroCnt++;
-            }
-            else if(nums[i]==1){
-                oneCnt++;
-            }else{
-                twoCnt++;
+        int largest = Integer.MIN_VALUE;
+        for(int i=0 ; i<nums.length ; i++){
+            if(nums[i] > largest){
+                largest = nums[i];
             }
         }
-        int ArrayIterator = 0;
-        if(ArrayIterator<nums.length){
-            while(zeroCnt>=1){
-                nums[ArrayIterator] = 0;
-                ArrayIterator++;
-                zeroCnt--;
-            }
-            while(oneCnt>=1){
-                nums[ArrayIterator] = 1;
-                ArrayIterator++;
-                oneCnt--;
-            }
-            while(twoCnt>=1){
-                nums[ArrayIterator] = 2;
-                ArrayIterator++;
-                twoCnt--;
+        int count[] = new int[largest+1];
+        for(int i=0 ; i<nums.length ; i++){
+            count[nums[i]]++;
+        }
+
+        int j =0 ;
+        for(int i=0 ; i<count.length ; i++){
+            while(count[i] >0){
+                nums[j] = i;
+                j++;
+                count[i]--;
             }
         }
     }
