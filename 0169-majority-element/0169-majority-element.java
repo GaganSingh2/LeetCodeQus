56b716013c1cb93df8f072461cc388577147ef17
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2025-03-20 10:23:53 +0530

    Time: 2 ms (75.13%), Space: 53.5 MB (21.25%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,60 +1,81 @@
-class Solution {
-    public int majorityElement(int[] nums) {
-        int count = 1;
-        int len = nums.length;
-        int res = 0;
-        // Arrays.sort(nums);
-        for(int i=0; i<=nums.length-1; i++){
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-            }
-            if(count > len/2){
-                return nums[i];
-            }
-            count = 1;
-        }
-        if(count > len/2){
-            res = nums[len-1];
-        }
-        return res;
-    }
-}
 // class Solution {
 //     public int majorityElement(int[] nums) {
-//         return majorityELementRec(nums,0,nums.length-1);
-//     }
-//     private static int countInRange(int nums[],int num, int lo,int hi){
-//         int count = 0;
-//         for(int i=lo; i<=hi; i++){
-//             if (nums[i]==num) {
-//                 count++;
+//         int count = 1;
+//         int len = nums.length;
+//         int res = 0;
+//         // Arrays.sort(nums);
+//         for(int i=0; i<=nums.length-1; i++){
+//             for(int j=i+1; j<nums.length; j++){
+//                 if(nums[i]==nums[j]){
+//                     count++;
+//                 }
 //             }
+//             if(count > len/2){
+//                 return nums[i];
+//             }
+//             count = 1;
 //         }
-//         return count;
-//     }
-//     private static int majorityELementRec(int nums[],int lo,int hi){
-//         //Base case
-//         if(lo==hi){
-//             return nums[lo];
+//         if(count > len/2){
+//             res = nums[len-1];
 //         }
-
-//         //Divide
-//         int mid = lo+(hi-lo)/2;
-//         int left = majorityELementRec(nums, lo, mid);//left part
-//         int right = majorityELementRec(nums, mid+1, hi);//right part
-
-//         //if the two halves agree on the mmajority element,return it
-//         if(left==right){
-//             return left;
-//         }
-
-//         //Otherwise, count each element and return the "winners"
-//         int leftCount = countInRange(nums, left, lo, hi);//left parrt
-//         int rightCount = countInRange(nums, right, lo, hi);
-
-//         return leftCount>rightCount ? left:right;
-
+//         return res;
 //     }
 // }
+// 
+
+class Solution{
+    public int majorityElement(int[] nums){
+       return majorityElementFin(nums,0,nums.length-1);
+    }
+
+    private static int countVal(int nums[],int val,int si,int ei){
+        int count = 0;
+        for(int i=si; i<=ei; i++){
+            if(nums[i]==val){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static int majorityElementFin(int nums[],int si, int ei){
+        // Base case
+        if(si==ei){
+            return nums[si];
+        }
+
+        int mid = si + (ei-si)/2;
+        int leftPart = majorityElementFin(nums,si,mid);
+        int rightPart = majorityElementFin(nums,mid+1,ei);
+
+        if(leftPart == rightPart){
+            return leftPart;
+        }
+
+        int leftCount = countVal(nums,leftPart,si,ei);
+        int rightCount = countVal(nums,rightPart,si,ei);
+
+        return leftCount > rightCount? leftPart : rightPart;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
