9396dcde6003ca541d7330b4d478caf1edceeae0
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2025-03-30 16:12:10 +0530

    Time: 1 ms (100%), Space: 42.8 MB (100%) - LeetHub

---
+++ 3811-reverse-degree-of-a-string/3811-reverse-degree-of-a-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int res = 0;
+        
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            int idx = 'z'-ch;
+            // System.out.println(idx);
+            res += (i+1)*(idx+1);
+        }
+        return res;
+    }
+}
