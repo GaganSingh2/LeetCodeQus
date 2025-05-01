ef0e6f7ee3ca04de401baea47f5beee878b62d5b
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2025-05-01 23:21:00 +0530

    Time: 0 ms (100%), Space: 42.1 MB (47.22%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,57 +1,53 @@
-// class Solution {
-//     public void sortColors(int[] nums) {
-//         int zeroCnt=0, oneCnt = 0, twoCnt = 0;
-//         for(int i=0; i<nums.length; i++){
-//             if(nums[i]==0){
-//                 zeroCnt++;
-//             }
-//             else if(nums[i]==1){
-//                 oneCnt++;
-//             }else{
-//                 twoCnt++;
-//             }
-//         }
-//         int ArrayIterator = 0;
-//         if(ArrayIterator<nums.length){
-//             while(zeroCnt>=1){
-//                 nums[ArrayIterator] = 0;
-//                 ArrayIterator++;
-//                 zeroCnt--;
-//             }
-//             while(oneCnt>=1){
-//                 nums[ArrayIterator] = 1;
-//                 ArrayIterator++;
-//                 oneCnt--;
-//             }
-//             while(twoCnt>=1){
-//                 nums[ArrayIterator] = 2;
-//                 ArrayIterator++;
-//                 twoCnt--;
-//             }
-//         }
-//     }
-// }
-
 class Solution {
     public void sortColors(int[] nums) {
-        int largest = Integer.MIN_VALUE;
-        for(int i=0 ; i<nums.length ; i++){
-            if(nums[i] > largest){
-                largest = nums[i];
-            }
-        }
-        int count[] = new int[largest+1];
-        for(int i=0 ; i<nums.length ; i++){
-            count[nums[i]]++;
+        int zeroCnt = 0, oneCnt = 0, twoCnt = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==0) zeroCnt++;
+            else if(nums[i]==1) oneCnt++;
+            else twoCnt++;
         }
 
-        int j =0 ;
-        for(int i=0 ; i<count.length ; i++){
-            while(count[i] >0){
-                nums[j] = i;
-                j++;
-                count[i]--;
+        int idx = 0;
+        if(idx<nums.length){
+            while(zeroCnt>=1){  //for Zero's
+                nums[idx] = 0;
+                idx++;
+                zeroCnt--;
+            }
+            while(oneCnt>=1){  //for One's
+                nums[idx] = 1;
+                idx++;
+                oneCnt--;
+            }
+            while(twoCnt>=1){  //for Two's
+                nums[idx] = 2;
+                idx++;
+                twoCnt--;
             }
         }
     }
 }
+
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         int largest = Integer.MIN_VALUE;
+//         for(int i=0 ; i<nums.length ; i++){
+//             if(nums[i] > largest){
+//                 largest = nums[i];
+//             }
+//         }
+//         int count[] = new int[largest+1];
+//         for(int i=0 ; i<nums.length ; i++){
+//             count[nums[i]]++;
+//         }
+
+//         int j =0 ;
+//         for(int i=0 ; i<count.length ; i++){
+//             while(count[i] >0){
+//                 nums[j] = i;
+//                 j++;
+//                 count[i]--;
+//             }
+//         }
+//     }
+// }
