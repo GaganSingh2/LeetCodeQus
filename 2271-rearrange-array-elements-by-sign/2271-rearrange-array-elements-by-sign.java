e6592cc8c611dc8b47c91debcb2e1081617e849d
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2025-05-02 22:25:03 +0530

    Time: 3 ms (100%), Space: 77.4 MB (39.06%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,23 +1,40 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        ArrayList<Integer> pos = new ArrayList<>();
-        ArrayList<Integer> neg = new ArrayList<>();
+        //---------------
+        // ArrayList<Integer> pos = new ArrayList<>();
+        // ArrayList<Integer> neg = new ArrayList<>();
 
+        // for(int i=0; i<nums.length; i++){
+        //     if(nums[i]<0){
+        //         neg.add(nums[i]);
+        //     }
+        //     else if(nums[i]>0){
+        //         pos.add(nums[i]);
+        //     }
+        // }
+        // int ite = 0;
+        // for(int i=0,j=0; i<pos.size() && j<neg.size();j++, i++){
+        //      nums[ite] = pos.get(i);
+        //      ite++;
+        //      nums[ite] = neg.get(j);
+        //      ite++;
+        // }
+        // return nums;
+
+        // -----------------
+        int pos = 0;
+        int neg = 1;
+        int res[] = new int[nums.length];
         for(int i=0; i<nums.length; i++){
-            if(nums[i]<0){
-                neg.add(nums[i]);
+            if(nums[i]>0){
+                res[pos] = nums[i];
+                pos += 2;
             }
-            else if(nums[i]>0){
-                pos.add(nums[i]);
+            else{
+                res[neg] = nums[i];
+                neg += 2;
             }
         }
-        int ite = 0;
-        for(int i=0,j=0; i<pos.size() && j<neg.size();j++, i++){
-             nums[ite] = pos.get(i);
-             ite++;
-             nums[ite] = neg.get(j);
-             ite++;
-        }
-        return nums;
+        return res;
     }
 }
