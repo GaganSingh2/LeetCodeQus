7835ee901d3b670c19cc67ddef6a9cc39e8123e2
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2025-06-23 12:49:42 +0530

    Time: 3 ms (41.19%), Space: 42.3 MB (84.09%) - LeetHub

--- 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.java
+++ 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.java
@@ -4,22 +4,22 @@ class Solution {
         int size = (int)Math.ceil((double)len/k);
 
         String res[] = new String[size];
-        for(int i=0,j=0; i<len; i+=k,j++){
-            res[j] = s.substring(i,Math.min(len,i+k));
+        for(int i=0,j=0; i<len; i+=k, j++){
+            res[j] = s.substring(i,Math.min(len,k+i));
         }
-        if(res[size-1].length() == k){
+
+        if(res[size-1].length()==k){
             return res;
         }
         else{
             StringBuilder sb = new StringBuilder(res[size-1]);
-            int si = res[size-1].length();
-            
-            for(int i=0; i<k-si; i++){
+            int si = k - res[size-1].length();
+            for(int i=0; i<si; i++){
                 sb.append(fill);
             }
-            res[size-1] = sb.toString();
+            res[res.length-1] = sb.toString();
         }
-        // System.out.println(sb.toString());
+        System.out.println(res[size-1].length());
         return res;
     }
 }
