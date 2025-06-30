a174522f8555700051ceb63e20b58fdad47b299c
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2025-06-30 10:37:05 +0530

    Time: 2 ms (54.81%), Space: 46.1 MB (60.47%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,49 +1,49 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length, n = nums2.length;
-        int res[] = new int[m+n];
-        int i = 0,j=0,idx=0;
-        while(i<m && j<n){
+        int temp[] = new int[m+n];
+        int i = 0, j = 0, idx = 0;
+        while(i<nums1.length && j<nums2.length){
             if(nums1[i]<nums2[j]){
-                res[idx] = nums1[i];
-                idx++;
+                temp[idx] = nums1[i];
                 i++;
+                idx++;
             }
             else if(nums1[i]==nums2[j]){
-                res[idx] = nums1[i];
-                idx++;
+                temp[idx] = nums1[i];
                 i++;
-                res[idx] = nums2[j];
                 idx++;
+                temp[idx] = nums2[j];
                 j++;
+                idx++;
             }
             else{
-                res[idx] = nums2[j];
-                idx++;
+                temp[idx] = nums2[j];
                 j++;
+                idx++;
             }
         }
         if(i<m){
             for(int k=i; k<m; k++){
-                res[idx] = nums1[k];
+                temp[idx] = nums1[k];
                 idx++;
             }
-        }else{
-            for(int k=j; k<n; k++){
-                res[idx] = nums2[k];
+        }
+        else{
+            for(int l=j; l<n; l++){
+                temp[idx] = nums2[l];
                 idx++;
             }
         }
         double ans = 0;
         int id1 = (m+n)/2;
         if((m+n)%2==0){
-            int id2 = ((m+n)/2)-1;
-            
-            ans = (double)(res[id1]+res[id2])/2;
-        }else{
-            ans = res[id1];
+            int id2 = (m+n)/2-1;
+            ans = (double)(temp[id1]+temp[id2])/2;
         }
-
+        else{
+            ans = temp[id1];
+        }
         return ans;
     }
 }
