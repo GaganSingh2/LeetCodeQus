efc9ac11d115e3b41557301749327b56fd6849f3
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2025-07-05 13:02:08 +0530

    Time: 1 ms (99.94%), Space: 44 MB (5.94%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
@@ -1,29 +1,44 @@
 class Solution {
     public int findLucky(int[] arr) {
-        Arrays.sort(arr);
-        // for(int i=0; i<arr.length; i++){
-        //     System.out.print(arr[i]+" ");
-        // }
-        int count = 1;
-        int res = -1;
-        for(int i=1; i<=arr.length-1; i++){
-            if(arr[i]==arr[i-1]){
-                count++;
-            }
-            else{
-                if(count==arr[i-1]){
-                    res = Math.max(res,arr[i-1]);
-                    count = 1;
-                }
-                else{
-                    count = 1;
-                }
-            }
 
+        //1st Way---------------
+        // Arrays.sort(arr);
+        // // for(int i=0; i<arr.length; i++){
+        // //     System.out.print(arr[i]+" ");
+        // // }
+        // int count = 1;
+        // int res = -1;
+        // for(int i=1; i<=arr.length-1; i++){
+        //     if(arr[i]==arr[i-1]){
+        //         count++;
+        //     }
+        //     else{
+        //         if(count==arr[i-1]){
+        //             res = Math.max(res,arr[i-1]);
+        //             count = 1;
+        //         }
+        //         else{
+        //             count = 1;
+        //         }
+        //     }
+
+        // }
+        // if(count == arr[arr.length-1]){
+        //     res = Math.max(res,arr[arr.length-1]);
+        // }
+        // return res;
+
+        //2nd Way------------------
+        int temp[] = new int[501];
+        for(int num: arr){
+            temp[num]++;
         }
-        if(count == arr[arr.length-1]){
-            res = Math.max(res,arr[arr.length-1]);
+
+        for(int i=500; i>0; i--){
+            if(temp[i]==i){
+                return i;
+            }
         }
-        return res;
+        return -1;
     }
 }
