e7b95f6cd19659e52399a805c86921aae4b0b0b6
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2025-07-17 16:43:05 +0530

    Time: 2 ms (80.43%), Space: 46.8 MB (9.05%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
          //1st Way Optimize
-       int left = 0;
-       for(int right=0; right<nums.length; right++){
+        int left = 0;
+        for(int right=0; right<nums.length; right++){
             if(nums[right] != 0){
                 int temp = nums[right];
                 nums[right] = nums[left];
                 nums[left] = temp;
                 left++;
             }
-       }
+        }
 
 
         //2nd Way
