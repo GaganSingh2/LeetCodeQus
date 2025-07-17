f5a71b965c45aaa1f5396f255e4eb067e384dd7a
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2025-07-17 17:25:01 +0530

    Time: 0 ms (100%), Space: 44.3 MB (39.79%) - LeetHub

--- 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.java
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.java
@@ -1,11 +1,23 @@
-import java.math.BigInteger;
+// import java.math.BigInteger;
 class Solution {
     public int arraySign(int[] nums) {
-        BigInteger pro =  BigInteger.ONE;
+        //1st Way-----
+        int sign = 1;
         for(int i=0; i<nums.length; i++){
-            pro = pro.multiply(BigInteger.valueOf(nums[i]));
+            if(nums[i]==0){
+                return 0;
+            }
+            if(nums[i]<0){
+                sign *= -1;
+            }
         }
-        int val = pro.compareTo(BigInteger.ZERO);
-        return (val>0)?1: (val<0)?-1:0;
+        return sign;
+        //2nd Way--------
+        // BigInteger pro =  BigInteger.ONE;
+        // for(int i=0; i<nums.length; i++){
+        //     pro = pro.multiply(BigInteger.valueOf(nums[i]));
+        // }
+        // int val = pro.compareTo(BigInteger.ZERO);
+        // return (val>0)?1: (val<0)?-1:0;
     }
 }
