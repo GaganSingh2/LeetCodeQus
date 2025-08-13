daa624ef860235560bfb7aed72810363adda0fd4
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2025-08-13 21:15:21 +0530

    Time: 8 ms (84.41%), Space: 44.1 MB (67.49%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,10 +1,21 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if(n<=0){
-            return false;
+
+        //Mathematical Approach------
+        // if(n<=0){
+        //     return false;
+        // }
+        // int maxPowerOfThree = 1162261467; // 3^19, the largest power of 3 within the int range
+        // // System.out.println(1162261467 / n);
+        // return maxPowerOfThree % n == 0;
+
+        //Iteration Approach
+        if(n==1) return true;
+        long temp = 1;
+        while(temp<n){
+            temp *= 3;
+            if(temp==n) return true;
         }
-        int maxPowerOfThree = 1162261467; // 3^19, the largest power of 3 within the int range
-        // System.out.println(1162261467 / n);
-        return maxPowerOfThree % n == 0;
+        return false;
     }
 }
