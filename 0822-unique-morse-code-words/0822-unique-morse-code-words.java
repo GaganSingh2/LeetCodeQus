d94a99d3ae71ae01b0da6e724c94145638529796
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2025-08-19 01:06:49 +0530

    Time: 4 ms (9.24%), Space: 42 MB (31.31%) - LeetHub

---
+++ 0822-unique-morse-code-words/0822-unique-morse-code-words.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        HashMap<Character,String> mp = new HashMap<>();
+        mp.put('a',".-");
+        mp.put('b',"-...");
+        mp.put('c',"-.-.");
+        mp.put('d',"-..");
+        mp.put('e',".");
+        mp.put('f',"..-.");
+        mp.put('g',"--.");
+        mp.put('h',"....");
+        mp.put('i',"..");
+        mp.put('j',".---");
+        mp.put('k',"-.-");
+        mp.put('l',".-..");
+        mp.put('m',"--");
+        mp.put('n',"-.");
+        mp.put('o',"---");
+        mp.put('p',".--.");
+        mp.put('q',"--.-");
+        mp.put('r',".-.");
+        mp.put('s',"...");
+        mp.put('t',"-");
+        mp.put('u',"..-");
+        mp.put('v',"...-");
+        mp.put('w',".--");
+        mp.put('x',"-..-");
+        mp.put('y',"-.--");
+        mp.put('z',"--..");
+
+       
+        Map<String,Integer> mp2 = new HashMap<>();
+        int cnt = 0;
+        for(int i=0; i<words.length; i++){
+            StringBuilder sb = new StringBuilder("");
+            for(int j=0; j<words[i].length(); j++){
+                sb.append(mp.getOrDefault(words[i].charAt(j),""));
+            }
+            mp2.put(sb.toString(),mp2.getOrDefault(sb.toString(),0)+1);
+            // cnt++;
+        }
+        for (Map.Entry<String, Integer> entry : mp2.entrySet()) {
+            // System.out.println(entry.getKey() + " -> " + entry.getValue());
+            cnt++;
+        }
+        return cnt;
+    }
+}
