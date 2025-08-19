ba7b318f1dfacc542ea049311114371ba70f8efe
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2025-08-19 23:35:41 +0530

    Time: 3 ms (94.03%), Space: 61.5 MB (81.82%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.java
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.java
@@ -10,29 +10,16 @@ class Solution {
         long res = 0;
         int cnt = 0;
         for(int i=0; i<nums.length; i++){
-        //     if(nums[i-1]==0 && nums[i]==0){
-        //         cnt++;
-        //     }
-        //    else if((nums[i-1]==0 && nums[i]!=0) || (nums[i-1]!=0 && nums[i]==0)){
-                
-        //    }
-        //     else{
-        //         if(cnt>0){
-        //             res += count(cnt);
-        //             // System.out.println(res);
-        //              cnt = 0;
-        //         }
-               
-        //     }
-               if(nums[i]==0){
+        
+            if(nums[i]==0){
                 cnt++;
-               }
-               else{
+            }
+            else{
                 if(cnt>0){
                     res += count(cnt);
                     cnt = 0;
                 }
-               }
+            }
         }
         if(cnt>0){
             res += count(cnt);
