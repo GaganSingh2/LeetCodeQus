c72a6fc1cbeb1f2e5431fc2843c53346bb321f7d
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2025-08-27 23:05:05 +0530

    Time: 2569 ms (5.06%), Space: 45.3 MB (32.14%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,35 @@
+class Solution {
+    private static boolean isPalindrome(String str){
+        int si = 0, ei = str.length()-1;
+        while(si<=ei){
+            if(str.charAt(si)!=str.charAt(ei)){
+                return false;
+            }
+            si++;
+            ei--;
+        }
+        return true;
+    }
+    
+    public String longestPalindrome(String s) {
+        // if(s.length()==1){
+        //     return s;
+        // }
+        StringBuilder sb = new StringBuilder("");
+        int len = sb.length();
+        for(int i=0; i<=s.length()-1; i++){
+            for(int j=i; j<=s.length(); j++){
+                String newStr = s.substring(i,j);
+                int curLen = newStr.length();
+                if(isPalindrome(newStr)){
+                    if(curLen>len){
+                        sb.setLength(0);
+                        sb.append(newStr);
+                        len = curLen;
+                    }
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
