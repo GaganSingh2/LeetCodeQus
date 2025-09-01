f557161a2dbfb4ebd443eef1d3baa880e93fbe86
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2025-09-01 21:39:40 +0530

    Time: 2 ms (0%), Space: 44.8 MB (100%) - LeetHub

--- 4008-restore-finishing-order/4008-restore-finishing-order.java
+++ 4008-restore-finishing-order/4008-restore-finishing-order.java
@@ -2,6 +2,9 @@ class Solution {
     public int[] recoverOrder(int[] order, int[] friends) {
         int newArr[] = new int[friends.length];
         int id = 0;
+
+        //1st Way----- TC: O(n^2), SC: O(n)
+        /*
         for(int i=0; i<order.length; i++){
             for(int j=0; j<friends.length; j++){
                 if(order[i]==friends[j]){
@@ -9,6 +12,20 @@ class Solution {
                     id++;
                     break;
                 }
+            }
+        }
+        return newArr;
+        */
+
+        //2nd Way-----------
+        Set<Integer> frndSet = new HashSet<>();
+        for(int frndId: friends){
+            frndSet.add(frndId);
+        }
+        for(int ele: order){
+            if(frndSet.contains(ele)){
+                newArr[id] = ele;
+                id++;
             }
         }
         return newArr;
