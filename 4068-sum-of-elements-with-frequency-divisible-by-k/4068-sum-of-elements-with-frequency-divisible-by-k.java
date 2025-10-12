6fdc16d100c2910228316bb7163e49068da65145
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2025-10-12 17:26:13 +0530

    Time: 3 ms (0%), Space: 43.2 MB (0%) - LeetHub

--- 4068-sum-of-elements-with-frequency-divisible-by-k/4068-sum-of-elements-with-frequency-divisible-by-k.java
+++ 4068-sum-of-elements-with-frequency-divisible-by-k/4068-sum-of-elements-with-frequency-divisible-by-k.java
@@ -1,23 +1,38 @@
 class Solution {
     public int sumDivisibleByK(int[] nums, int k) {
-        
-        Arrays.sort(nums);
-        int sum = 0;
-        int freq = 1;
-        for(int i=1; i<=nums.length-1; i++){
-            if(nums[i-1]==nums[i]){
-                freq++;
-            }
-            else{
-                if(freq % k == 0){
-                    sum += freq * nums[i-1];
-                }
-                freq = 1;
-            }
+        //1st Way---------TC->O(n), SC->O(n)---
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i: nums){
+            map.put(i,map.getOrDefault(i,0)+1);
         }
-        if(freq%k==0){
-            sum += freq * nums[nums.length-1]; 
+        int sum = 0;
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            int val = entry.getKey();
+            int count = entry.getValue();
+            if(count%k==0){
+                sum += val * count;
+            }
         }
         return sum;
+
+        //2nd way----------TC->O(nlogn), SC->O(1)--------
+        // Arrays.sort(nums);
+        // int sum = 0;
+        // int freq = 1;
+        // for(int i=1; i<=nums.length-1; i++){
+        //     if(nums[i-1]==nums[i]){
+        //         freq++;
+        //     }
+        //     else{
+        //         if(freq % k == 0){
+        //             sum += freq * nums[i-1];
+        //         }
+        //         freq = 1;
+        //     }
+        // }
+        // if(freq%k==0){
+        //     sum += freq * nums[nums.length-1]; 
+        // }
+        // return sum;
     }
 }
