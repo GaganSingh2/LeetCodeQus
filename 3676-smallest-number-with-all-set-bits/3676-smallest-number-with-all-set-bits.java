39231ad8220dff1c686818b6cc960cdbff7d72d5
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2025-10-30 09:50:08 +0530

    Time: 0 ms (100%), Space: 40.8 MB (58.08%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
@@ -1,37 +1,42 @@
 class Solution {
     public int smallestNumber(int n) {
-        int binaryVal = 0, tmp = n, pow = 0, cnt = 0;
-        while(tmp>0){
-            int rem = tmp%2;
-            binaryVal = binaryVal + (rem * (int)Math.pow(10,pow));
-            cnt++;
-            pow++;
-            tmp /= 2;
+        int x = n;
+        while((x & x+1) != 0){
+            x++;
         }
-        // System.out.println(binaryVal);
-        int res = (int)Math.pow(2,cnt)-1;
-        tmp = res;
-         binaryVal = 0;
-          pow = 0;
-        int cntSet = 0, cntNonSet = 0;
-        while(tmp>0){
-            int rem = tmp%2;
-            binaryVal = binaryVal + (rem * (int)Math.pow(10,pow));
-            if(rem==1){
-                cntSet++;
-                 pow++;
-                tmp /= 2;
-            }
-            else{
-                tmp = res-1;
-                res--;
-                binaryVal = 0;
-                 pow = 0; cntSet = 0;
-                  cntNonSet = 0;
-            }
-        }
-        
-       
-        return res;
+        return x;
+
+        // ----------------------------------------------------------------------------
+        // int binaryVal = 0, tmp = n, pow = 0, cnt = 0;
+        // while(tmp>0){
+        //     int rem = tmp%2;
+        //     binaryVal = binaryVal + (rem * (int)Math.pow(10,pow));
+        //     cnt++;
+        //     pow++;
+        //     tmp /= 2;
+        // }
+        // // System.out.println(binaryVal);
+        // int res = (int)Math.pow(2,cnt)-1;
+        // tmp = res;
+        //  binaryVal = 0;
+        //   pow = 0;
+        // int cntSet = 0, cntNonSet = 0;
+        // while(tmp>0){
+        //     int rem = tmp%2;
+        //     binaryVal = binaryVal + (rem * (int)Math.pow(10,pow));
+        //     if(rem==1){
+        //         cntSet++;
+        //          pow++;
+        //         tmp /= 2;
+        //     }
+        //     else{
+        //         tmp = res-1;
+        //         res--;
+        //         binaryVal = 0;
+        //          pow = 0; cntSet = 0;
+        //           cntNonSet = 0;
+        //     }
+        // }
+        // return res;
     }
 }
