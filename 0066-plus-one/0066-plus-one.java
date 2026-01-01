c8bd3e547e193b93df4dca87e12a1888beb2500f
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-01-01 18:39:49 +0530

    Time: 0 ms (100%), Space: 43.1 MB (89.12%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -5,10 +5,10 @@ class Solution {
 
             if (digits[i] < 9) {
                 digits[i]++;
-                return digits;
+                return digits; //if last digit is less than 9 then +1 and return the result
             }
 
-            digits[i] = 0;
+            digits[i] = 0; //add 0 at last 
         }
 
         int[] result = new int[digits.length + 1];
