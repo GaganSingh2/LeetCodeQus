2786172106151da8036fac95a158752fb4c6651e
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-01-10 01:58:45 +0530

    Time: 0 ms (100%), Space: 42.9 MB (23.51%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,97 +1,43 @@
 class MyStack {
-    private Queue<Integer> first;
-    private Queue<Integer> second;
+    Queue<Integer> fir;
+    Queue<Integer> sec;
     public MyStack() {
-        first = new LinkedList<>();
-        second = new LinkedList<>();
+        fir = new LinkedList<>();
+        sec = new LinkedList<>();
     }
-    
-    // 1st Approach----
-    // public void push(int x) { //O(n)
-    //     while(!first.isEmpty()){
-    //         second.add(first.poll());
-    //     }
-    //     first.add(x);
-    //     while(!second.isEmpty()){
-    //         first.add(second.poll());
-    //     }
-    // }
-    
-    // public int pop() {  //O(1)
-    //     return first.poll();
-    // }
-    
-    // public int top() {  //O(1)
-    //     return first.peek();
-    // }
-    
-    // public boolean empty() {
-    //     return first.isEmpty();
-    // }
 
-    // 2nd Approach-----
+    // 1st Approach----- Push take O(n) and pop take O(1)
     public boolean empty(){
-        return first.isEmpty() && second.isEmpty();
+        return fir.isEmpty();
     }
 
-    // Add O(1)
+    //TC: O(n)
     public void push(int x){
-        if(!first.isEmpty()){
-            first.add(x);
-        }else{
-            second.add(x);
+        while(!fir.isEmpty()){
+            sec.add(fir.remove());
+        }
+        fir.add(x);
+        while(!sec.isEmpty()){
+            fir.add(sec.remove());
         }
     }
 
-    // Pop O(n)
+    //TC: O(1)
     public int pop(){
-        if(empty()){
+       if(empty()){
             return -1;
-        }
-        int top = -1;
-        // if Queue1 is Non-empty then push the whole data in queue2
-        if(!first.isEmpty()){
-            while(!first.isEmpty()){
-                top = first.poll();
-                if(first.isEmpty()){
-                    break;
-                }
-                second.add(top);
-            }
-        }
-        else{
-            while(!second.isEmpty()){
-                top = second.poll();
-                if(second.isEmpty()){
-                    break;
-                }
-                first.add(top);
-            }
-        }
-        return top;
+       }
+       return fir.remove();
     }
 
-    // Peek O(n)
+    
     public int top(){
-        if(empty()){
+       if(empty()){
             return -1;
-        }
-        int top = -1;
-        // if Queue1 is Non-empty then push the whole data in queue2
-        if(!first.isEmpty()){
-            while(!first.isEmpty()){
-                top = first.poll();
-                second.add(top);
-            }
-        }
-        else{
-            while(!second.isEmpty()){
-                top = second.poll();
-                first.add(top);
-            }
-        }
-        return top;
+       }
+       return fir.peek();
     }
+
 
 }
 
@@ -103,3 +49,9 @@ class MyStack {
  * int param_3 = obj.top();
  * boolean param_4 = obj.empty();
  */
+
+ /**
+ 1 2 3=> 3 2 1 stk
+ q1 = 3 2 1
+ q2 = 2 1
+  */
