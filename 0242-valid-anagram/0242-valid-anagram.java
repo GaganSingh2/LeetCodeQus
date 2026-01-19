5cdce1eb6956cb9b7083e5cac87e7f60cfb13424
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-01-19 23:36:03 +0530

    Time: 19 ms (12.27%), Space: 47.1 MB (6.91%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,54 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
-        }
-        int temp[] = new int[26];
-        for(int i=0; i<s.length(); i++){
-            char ch1 = s.charAt(i);
-            char ch2 = t.charAt(i);
-            temp[ch1-'a'] += 1;
-            temp[ch2-'a'] -= 1;
-        }
 
-        for(int i=0; i<26; i++){
-           if(temp[i] != 0){
+       //1st Approach
+       if(s.length() != t.length()){
+            return false;
+       }
+       LinkedHashMap<Character,Integer> map = new LinkedHashMap<>();
+       for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(map.containsKey(ch)){
+                map.put(ch,map.get(ch)+1);
+            }
+            else{
+                map.put(ch,1);
+            }
+        }
+       
+        for(int i=0; i<t.length(); i++){
+            char ch = t.charAt(i);
+            if(map.containsKey(ch)){
+                map.put(ch,map.get(ch)-1);
+            }
+            else{
+                map.put(ch,1);
+            }
+        }
+        for(Character k: map.keySet()){
+            if(map.get(k) != 0){
                 return false;
-           }
+            }
         }
         return true;
+
+        //2nd Approach
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
+        // int temp[] = new int[26];
+        // for(int i=0; i<s.length(); i++){
+        //     char ch1 = s.charAt(i);
+        //     char ch2 = t.charAt(i);
+        //     temp[ch1-'a'] += 1;
+        //     temp[ch2-'a'] -= 1;
+        // }
+
+        // for(int i=0; i<26; i++){
+        //    if(temp[i] != 0){
+        //         return false;
+        //    }
+        // }
+        // return true;
     }
 }
