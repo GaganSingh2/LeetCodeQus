461c21f423beb7447053fc99650d5abf41e59125
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-01-22 19:39:23 +0530

    Time: 3 ms (66.06%), Space: 44.5 MB (46.87%) - LeetHub

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.java
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.java
@@ -1,32 +1,32 @@
 class Solution {
     public int minimumPairRemoval(int[] nums) {
         ArrayList<Integer> temp = new ArrayList<>();
-        for(int i=0;i<nums.length; i++){
-            temp.add(nums[i]);
+        for(int ele: nums){
+            temp.add(ele);
         }
-        int operation = 0;
+        int opt = 0;
         while(!isNonDec(temp)){
-            
-        
-        int minsum = Integer.MAX_VALUE;
-        int minIdx=-1;
-        for(int i=0;i<temp.size()-1; i++){
-           int sum = temp.get(i)+temp.get(i+1);
-            if(sum<minsum){
-                minsum = sum;
-                minIdx = i;
+
+            int minSum = Integer.MAX_VALUE;
+            int minIdx = -1;
+            for(int i=0; i<temp.size()-1; i++){
+                int sum = temp.get(i) + temp.get(i+1);
+                if(sum<minSum){
+                    minSum = sum;
+                    minIdx = i;
+                }
             }
+            int comb = temp.get(minIdx)+temp.get(minIdx+1);
+            temp.set(minIdx,comb);
+            temp.remove(minIdx+1);
+            opt++;
         }
-        int comb = temp.get(minIdx) + temp.get(minIdx+1);
-        temp.set(minIdx,comb);
-        temp.remove(minIdx+1);
-        operation++;
-        }
-        return operation;
+        return opt;
     }
-    private static boolean isNonDec(List<Integer> temp){
-        for(int i=1; i<temp.size(); i++){
-            if(temp.get(i)<temp.get(i-1)){
+
+    private static boolean isNonDec(List<Integer> li){
+        for(int i=1; i<li.size(); i++){
+            if(li.get(i)<li.get(i-1)){
                 return false;
             }
         }
