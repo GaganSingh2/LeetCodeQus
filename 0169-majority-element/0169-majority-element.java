3bd32eb90bd141ceb530e162d41e49d4fb18fe8a
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-01-31 00:37:35 +0530

    Time: 16 ms (19.68%), Space: 52.9 MB (75.54%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,3 +1,18 @@
+class Solution{
+    public int majorityElement(int[] nums){
+        int times = nums.length/2;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+            if(map.get(nums[i])>times){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
+
+
 // class Solution {
 //     public int majorityElement(int[] nums) {
 //         int count = 1;
@@ -23,41 +38,41 @@
 // }
 // 
 
-class Solution{
-    public int majorityElement(int[] nums){
-       return majorityElementFin(nums,0,nums.length-1);
-    }
 
-    private static int countVal(int nums[],int val,int si,int ei){
-        int count = 0;
-        for(int i=si; i<=ei; i++){
-            if(nums[i]==val){
-                count++;
-            }
-        }
-        return count;
-    }
 
-    private static int majorityElementFin(int nums[],int si, int ei){
-        // Base case
-        if(si==ei){
-            return nums[si];
-        }
 
-        int mid = si + (ei-si)/2;
-        int leftPart = majorityElementFin(nums,si,mid);
-        int rightPart = majorityElementFin(nums,mid+1,ei);
+// class Solution{
+    // public int majorityElement(int[] nums){
+//        return majorityElementFin(nums,0,nums.length-1);
+//     }
 
-        if(leftPart == rightPart){
-            return leftPart;
-        }
+//     private static int countVal(int nums[],int val,int si,int ei){
+//         int count = 0;
+//         for(int i=si; i<=ei; i++){
+//             if(nums[i]==val){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
 
-        int leftCount = countVal(nums,leftPart,si,ei);
-        int rightCount = countVal(nums,rightPart,si,ei);
+//     private static int majorityElementFin(int nums[],int si, int ei){
+//         // Base case
+//         if(si==ei) return nums[si];
 
-        return leftCount > rightCount? leftPart : rightPart;
-    }
-}
+//         int mid = si+(ei-si)/2;
+
+//         int leftPart = majorityElementFin(nums,si,mid);
+//         int rightPart = majorityElementFin(nums,mid+1,ei);
+       
+//         if(leftPart == rightPart) return leftPart;
+
+//         int leftCnt = countVal(nums,leftPart,si,ei);
+//         int rightCnt = countVal(nums,rightPart,si,ei);
+
+//         return leftCnt>rightCnt? leftPart: rightPart;
+//     }
+// }
 
 
 
