4387b9a0f44b312d8e650597aaa398db3f2a72fc
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-01-31 15:13:39 +0530

    Time: 43 ms (11.81%), Space: 47.6 MB (54.51%) - LeetHub

---
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mostFrequentEven(int[] nums) {
+        TreeMap<Integer,Integer> freq = new TreeMap<>();
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]%2==0){
+                freq.put(nums[i], freq.getOrDefault(nums[i], 0)+1);
+                // freq.put(nums[i], freq.get(nums[i])+1);
+            }
+            // if(nums[i])
+        }
+
+        int res = -1, times = 0;
+        for(Integer val: freq.keySet()){
+            if(freq.get(val)>times){
+                res = val;
+                times = freq.get(val);
+            }
+        } 
+        return res;
+    }
+}
