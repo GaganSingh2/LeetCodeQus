fd3d74c61430e054015b01a265aa9e81837ce420
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-02-04 23:22:26 +0530

    Time: 4 ms (95.96%), Space: 44.9 MB (76.52%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,31 +1,42 @@
 class Solution {
     public String simplifyPath(String path) {
-        String[] str = path.split("/");
-        Stack<String> stk = new Stack<>();
+        //1st Step
+        String str[] = path.split("/");
 
+        Stack<String> stk = new Stack<>();
+        //2nd Step
         for(int i=0; i<str.length; i++){
-            if(str[i].equals(".") || str[i].equals("")){
+            //if space("") and dot(.) then Ignore
+            if(str[i].equals("") || str[i].equals(".")){
                 continue;
             }
+            // if arr[i]==".." then pop from stack
             else if(str[i].equals("..")){
                 if(!stk.isEmpty()){
                     stk.pop();
                 }
-                else{
-                    continue;
-                }
             }
+            // Oterwise push into the stack
             else{
                 stk.push(str[i]);
             }
         }
-
         StringBuilder sb = new StringBuilder("");
-        for(String str1: stk){
-            sb.append("/").append(str1);
+        for(String st: stk){
+            sb.append("/").append(st);
         }
-        return (sb.length()>1)?sb.toString():"/";
+        return (sb.length()>1)?sb.toString(): "/";
     }
 }
-
-// 
+/*
+1step:
+->Split the path based on the '/'
+2nd Step:
+->if arr[i]==""(space) then ignore
+->if arr[i]=="." then ignore
+->if arr[i]==".." then pop from stack
+->else push into the stack
+3rd Step:
+->join all the path with '/'
+->start with '/'
+ */
