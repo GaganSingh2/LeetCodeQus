d0689dd10126128e335cb46d89fb32f943b24712
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-02-12 22:42:03 +0530

    Time: 1 ms (78.9%), Space: 44.8 MB (57.65%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.java
+++ 3637-trionic-array-i/3637-trionic-array-i.java
@@ -2,21 +2,24 @@ class Solution {
     public boolean isTrionic(int[] nums) {
         int len = nums.length;
         int i = 1;
+        //Find the end point of first strictly increasing segment
         while(i<len && nums[i-1]<nums[i]){
             i++;
         }
 
         int p = i-1;
+        //Find the end point of  strictly decreasing segment
         while(i<len && nums[i-1]>nums[i]){
             i++;
         }
         
         int q = i-1;
+        //Find the end point of last strictly increasing segment
         while(i<len && nums[i-1]<nums[i]){
             i++;
         }
         int flag = i-1;
-        
+       
         return (p != 0) && (q != p) && (flag == len-1 && flag != q);
     }
 }
