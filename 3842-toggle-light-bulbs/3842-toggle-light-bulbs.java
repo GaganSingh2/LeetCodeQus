7d781a0eabb002779ff0178ae91c688735f48ae8
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-02-23 00:17:28 +0530

    Time: 2 ms (99.98%), Space: 46.8 MB (65.62%) - LeetHub

--- 3842-toggle-light-bulbs/3842-toggle-light-bulbs.java
+++ 3842-toggle-light-bulbs/3842-toggle-light-bulbs.java
@@ -1,17 +1,33 @@
 class Solution {
     public List<Integer> toggleLightBulbs(List<Integer> bulbs) {
-        HashMap<Integer,Integer> freq = new HashMap<>();
-        for(int i=0; i<bulbs.size(); i++){
-            freq.put(bulbs.get(i), freq.getOrDefault(bulbs.get(i), 0)+1);
+
+        boolean track[] = new boolean[101];
+        for(int bulb: bulbs){
+            track[bulb] = !track[bulb];
         }
-        List<Integer> result = new ArrayList<>();
-        for(Map.Entry<Integer,Integer> entry: freq.entrySet()){
-            // System.out.println(entry.getKey()+" "+entry.getValue());
-            if(entry.getValue() % 2 == 1){
-                result.add(entry.getKey());
+
+        List<Integer> result = new ArrayList<Integer>();
+        for(int i=1; i<=100; i++){
+            if(track[i]){
+                result.add(i);
             }
         }
-        Collections.sort(result);
+        
         return result;
+
+
+        // HashMap<Integer,Integer> freq = new HashMap<>();
+        // for(int i=0; i<bulbs.size(); i++){
+        //     freq.put(bulbs.get(i), freq.getOrDefault(bulbs.get(i), 0)+1);
+        // }
+        // List<Integer> result = new ArrayList<>();
+        // for(Map.Entry<Integer,Integer> entry: freq.entrySet()){
+        //     // System.out.println(entry.getKey()+" "+entry.getValue());
+        //     if(entry.getValue() % 2 == 1){
+        //         result.add(entry.getKey());
+        //     }
+        // }
+        // Collections.sort(result);
+        // return result;
     }
 }
