bee075117c619fbe3b28eb174f95477bc7338f19
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-02-22 23:48:24 +0530

    Time: 2 ms (18.19%), Space: 47.1 MB (67.24%) - LeetHub

--- 3847-find-the-score-difference-in-a-game/3847-find-the-score-difference-in-a-game.java
+++ 3847-find-the-score-difference-in-a-game/3847-find-the-score-difference-in-a-game.java
@@ -1,33 +1,16 @@
 class Solution {
     public int scoreDifference(int[] nums) {
+
         int firPlayerPoints = 0, secPlayerPoints = 0;
         boolean firPlayerActive = true;
-        boolean secPlayerActive = false;
-        int cntGame = 0;
         for(int i=0; i<nums.length; i++){
-            cntGame++;
-            if(nums[i]%2!=0){
-                if(firPlayerActive){
-                    firPlayerActive = false;
-                    secPlayerActive = true;
-                }
-                else{
-                    firPlayerActive = true;
-                    secPlayerActive = false;
-                }
+            if(nums[i] % 2!=0){
+                firPlayerActive = !firPlayerActive;
             }
-            if(cntGame==6){
-                if(firPlayerActive){
-                    firPlayerActive = false;
-                    secPlayerActive = true;
-                    cntGame = 0;
-                }
-                else{
-                    firPlayerActive = true;
-                    secPlayerActive = false;
-                    cntGame = 0;
-                }
+            if(i % 6==5){
+                firPlayerActive = !firPlayerActive;
             }
+
             if(firPlayerActive){
                 firPlayerPoints += nums[i];
             }
@@ -35,7 +18,46 @@ class Solution {
                 secPlayerPoints += nums[i];
             }
         }
-        // System.out.println(firPlayerPoints+" "+secPlayerPoints);
         return firPlayerPoints - secPlayerPoints;
+
+
+
+        // int firPlayerPoints = 0, secPlayerPoints = 0;
+        // boolean firPlayerActive = true;
+        // boolean secPlayerActive = false;
+        // int cntGame = 0;
+        // for(int i=0; i<nums.length; i++){
+        //     cntGame++;
+        //     if(nums[i]%2!=0){
+        //         if(firPlayerActive){
+        //             firPlayerActive = false;
+        //             secPlayerActive = true;
+        //         }
+        //         else{
+        //             firPlayerActive = true;
+        //             secPlayerActive = false;
+        //         }
+        //     }
+        //     if(cntGame==6){
+        //         if(firPlayerActive){
+        //             firPlayerActive = false;
+        //             secPlayerActive = true;
+        //             cntGame = 0;
+        //         }
+        //         else{
+        //             firPlayerActive = true;
+        //             secPlayerActive = false;
+        //             cntGame = 0;
+        //         }
+        //     }
+        //     if(firPlayerActive){
+        //         firPlayerPoints += nums[i];
+        //     }
+        //     else{
+        //         secPlayerPoints += nums[i];
+        //     }
+        // }
+        // // System.out.println(firPlayerPoints+" "+secPlayerPoints);
+        // return firPlayerPoints - secPlayerPoints;
     }
 }
