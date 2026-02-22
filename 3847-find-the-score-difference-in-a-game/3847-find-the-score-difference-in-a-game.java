a3bca102d465df5e990223f89cdc374e8d5671ed
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-02-22 23:38:07 +0530

    Time: 1 ms (99.99%), Space: 47 MB (88.7%) - LeetHub

---
+++ 3847-find-the-score-difference-in-a-game/3847-find-the-score-difference-in-a-game.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int scoreDifference(int[] nums) {
+        int firPlayerPoints = 0, secPlayerPoints = 0;
+        boolean firPlayerActive = true;
+        boolean secPlayerActive = false;
+        int cntGame = 0;
+        for(int i=0; i<nums.length; i++){
+            cntGame++;
+            if(nums[i]%2!=0){
+                if(firPlayerActive){
+                    firPlayerActive = false;
+                    secPlayerActive = true;
+                }
+                else{
+                    firPlayerActive = true;
+                    secPlayerActive = false;
+                }
+            }
+            if(cntGame==6){
+                if(firPlayerActive){
+                    firPlayerActive = false;
+                    secPlayerActive = true;
+                    cntGame = 0;
+                }
+                else{
+                    firPlayerActive = true;
+                    secPlayerActive = false;
+                    cntGame = 0;
+                }
+            }
+            if(firPlayerActive){
+                firPlayerPoints += nums[i];
+            }
+            else{
+                secPlayerPoints += nums[i];
+            }
+        }
+        // System.out.println(firPlayerPoints+" "+secPlayerPoints);
+        return firPlayerPoints - secPlayerPoints;
+    }
+}
