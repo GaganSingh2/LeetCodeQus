a76720f6ef9f2605263fff4632f0f51c769e6c86
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-02-25 00:58:07 +0530

    Time: 1 ms (100%), Space: 43.7 MB (21.27%) - LeetHub

--- 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.java
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.java
@@ -4,13 +4,15 @@ class Solution {
         int sum = 0;
         for(int i=1; i<=nums.length-1; i++){
             sum = nums[i]+nums[i-1];
+            //if the sum is already present means this is the second time we got same sum so we found our result and return true
             if(set.contains(sum)){
                 return true;
             }
-            else{
+            else{ //otherwise add in the set as 1st time found
                 set.add(sum);
             }
         }
+        //not found
         return false;
     }
 }
