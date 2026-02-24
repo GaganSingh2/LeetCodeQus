9e9b88c907f3f96bf82788fd1126fd82355cd875
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-02-24 18:38:38 +0530

    Time: 2 ms (99.21%), Space: 47.4 MB (9.59%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,11 +1,23 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0; i<=nums.length-1; i++){
-            for(int j=i+1; j<nums.length; j++){
-                if((nums[i]+nums[j])==target){
-                    return new int[]{i,j};
-                }
+        //Using Nested Loop TC:O(n^2), SC:O(n)--------
+        // for(int i=0; i<=nums.length-1; i++){
+        //     for(int j=i+1; j<nums.length; j++){
+        //         if((nums[i]+nums[j])==target){
+        //             return new int[]{i,j};
+        //         }
+        //     }
+        // }
+        // return new int[]{};
+ 
+        //By Using HashMap TC:O(n), SC:O(n)-------
+        HashMap<Integer, Integer> pairIdx = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int num = nums[i];
+            if(pairIdx.containsKey(target-num)){
+                return new int[]{i, pairIdx.get(target-num)};
             }
+            pairIdx.put(num,i);
         }
         return new int[]{};
     }
