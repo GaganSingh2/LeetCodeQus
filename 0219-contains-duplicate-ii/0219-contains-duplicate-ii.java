10156104b9124495624afc893f01cbdf2ef19a1b
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-03-02 01:00:16 +0530

    Time: 23 ms (81.21%), Space: 90.7 MB (36.07%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -2,21 +2,22 @@ class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         HashMap<Integer,Integer> freq = new HashMap<>();
         for(int i=0; i<nums.length; i++){
+            //if map contains nums[i]
             if(freq.containsKey(nums[i])){
-                int num = Math.abs(freq.get(nums[i])-i);
-                if(num<=k){
+                //then found difference of currentIdx and prevIdx
+                int diff = Math.abs(freq.get(nums[i])-i);
+                //if diff is lessthan or equalto k then return true
+                if(diff<=k){
                     return true;
                 }
-                else{
+                else{ //otherwise update the idx in the map, may after some iteration this one is used
                     freq.put(nums[i], i);
                 }
             }
-            else{
+            else{ //otherwise add in map
                 freq.put(nums[i], i);
             }
         }
-        return false;
-
-       
+        return false;       
     }
 }
