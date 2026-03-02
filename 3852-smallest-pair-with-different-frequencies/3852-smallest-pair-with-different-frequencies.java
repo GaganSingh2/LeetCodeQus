f740aa954a6fa27c8298d7ef21d584758530b69e
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-03-02 12:58:09 +0530

    Time: 6 ms (100%), Space: 46.8 MB (100%) - LeetHub

--- 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.java
+++ 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.java
@@ -2,12 +2,15 @@ class Solution {
     public int[] minDistinctFreqPair(int[] nums) {
         TreeMap<Integer,Integer> freq = new TreeMap<>();
 
+        //find all the frequency and sort in Ascending order using TreeMap
         for(int i=0; i<nums.length; i++){
             freq.put(nums[i], freq.getOrDefault(nums[i], 0)+1);
         }
-        int num1 = -1;
-        num1 = (int)freq.keySet().toArray()[0];
+        
+        //store the first key in this variable
+        int num1 = (int)freq.keySet().toArray()[0];
         for(Integer i: freq.keySet()){
+            //check second key just greater than first key is available in the map if yes then return the result
             if(i>num1 && freq.get(num1) != freq.get(i)){
                 return new int[]{num1,i};
             }
