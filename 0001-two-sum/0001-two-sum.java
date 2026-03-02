c9fc2895ff9685c630cee155d156ac4200147da6
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-03-03 01:37:46 +0530

    Time: 2 ms (99.22%), Space: 47.2 MB (57.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -15,7 +15,7 @@ class Solution {
         for(int i=0; i<nums.length; i++){
             int num = nums[i];
             if(pairIdx.containsKey(target-num)){
-                return new int[]{i, pairIdx.get(target-num)};
+                return new int[]{pairIdx.get(target-num),i};
             }
             pairIdx.put(num,i);
         }
