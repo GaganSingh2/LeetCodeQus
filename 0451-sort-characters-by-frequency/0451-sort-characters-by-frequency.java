f9c4f37a6fcf7f115f8750146e7cf41f1f4ad1a3
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-03-03 12:34:22 +0530

    Time: 15 ms (48.82%), Space: 46.7 MB (65.98%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -17,5 +17,21 @@ class Solution {
             }
         }
         return res.toString();
+
+
+        // HashMap<Character,StringBuilder> freq = new HashMap<>();
+        // for(int i=0; i<s.length(); i++){
+        //     freq.put(s.charAt(i), freq.getOrDefault(s.charAt(i), new StringBuilder()).append(s.charAt(i)));
+        // }
+        
+        // PriorityQueue<Collection<StringBuilder>> maxFreq = new PriorityQueue<>((a,b)->b.size()-a.size());
+       
+        // maxFreq.addAll(freq.values());
+        //  System.out.println(maxFreq);
+        // StringBuilder res = new StringBuilder("");
+        // // while(!maxFreq.isEmpty()){
+            
+        // // }
+        // return res.toString();
     }
 }
