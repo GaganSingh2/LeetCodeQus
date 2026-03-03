c28cb276d752606ccdd71c5ef53f34da62a9cb6e
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-03-03 15:17:50 +0530

    Time: 5 ms (95.93%), Space: 45.8 MB (52.76%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -1,26 +1,58 @@
 class Solution {
     public int[] frequencySort(int[] nums) {
+        // //TC: O(nlogn), SC: O(n)=========================
+        // HashMap<Integer,Integer> freq = new HashMap<>();
+        // //Count the Frequency using HashMap
+        // for(int i=0; i<nums.length; i++){
+        //     freq.put(nums[i], freq.getOrDefault(nums[i], 0)+1);
+        // }
+        // //put Integer into PriorityQueue based on the frequency and if frequency is same then store in Decreasing order
+        // PriorityQueue<Integer> minFreq = new PriorityQueue<>((a,b)->{
+        //     int freqCompare = freq.get(a)-freq.get(b);
+        //     if(freqCompare != 0){
+        //         return freqCompare;
+        //     }
+        //     return b-a;
+        // });
+
+        // minFreq.addAll(freq.keySet());
+        // int res[] = new int[nums.length];
+        // int idx = 0;
+        // while(!minFreq.isEmpty()){
+        //     int val = minFreq.poll();
+        //     int len = freq.get(val);
+        //     while(len-->0){
+        //         res[idx++] = val;
+        //     }
+        // }
+        // return res;
+
+
+
+        //TC: O(n), SC: O(n)
         HashMap<Integer,Integer> freq = new HashMap<>();
         for(int i=0; i<nums.length; i++){
             freq.put(nums[i], freq.getOrDefault(nums[i], 0)+1);
         }
-
-        PriorityQueue<Integer> minFreq = new PriorityQueue<>((a,b)->{
-            int freqCompare = freq.get(a)-freq.get(b);
-            if(freqCompare != 0){
-                return freqCompare;
+        List<Integer> bucket[] = new List[nums.length+1];
+        for(Integer val: freq.keySet()){
+            Integer idx = freq.get(val);
+            if(bucket[idx] == null){
+                bucket[idx] = new ArrayList<>();
             }
-            return b-a;
-        });
+            bucket[idx].add(val);
+        }
 
-        minFreq.addAll(freq.keySet());
         int res[] = new int[nums.length];
         int idx = 0;
-        while(!minFreq.isEmpty()){
-            int val = minFreq.poll();
-            int len = freq.get(val);
-            while(len-->0){
-                res[idx++] = val;
+        for(int i=0; i<bucket.length; i++){
+            if(bucket[i] != null){
+                Collections.sort(bucket[i], Collections.reverseOrder());
+                for(int val: bucket[i]){
+                    for(int j=0; j<i; j++){
+                        res[idx++] = val;
+                    }
+                }
             }
         }
         return res;
