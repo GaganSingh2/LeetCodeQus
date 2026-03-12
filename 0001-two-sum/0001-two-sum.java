d5ed16160c18361ed182699fbc8f5961e2f1320d
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-03-12 23:09:27 +0530

    Time: 2 ms (99.22%), Space: 47.2 MB (36.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        //Using Nested Loop TC:O(n^2), SC:O(n)--------
+        //Using Nested Loop TC:O(n^2), SC:O(1)--------
         // for(int i=0; i<=nums.length-1; i++){
         //     for(int j=i+1; j<nums.length; j++){
         //         if((nums[i]+nums[j])==target){
