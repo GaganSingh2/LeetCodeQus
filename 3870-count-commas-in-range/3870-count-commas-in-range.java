be53b92440fbcc4bef0bed5fc523e9cf4b92ebde
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-03-16 02:10:12 +0530

    Time: 1 ms (0%), Space: 42.7 MB (100%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -4,8 +4,22 @@ class Solution {
             return 0;
         }
 
-        return n-999;
+        int totalComma = 0, start = 1000, commas = 1;
+        while(start<=n){
+            int end = start * 1000 -1;
+            int count = Math.min(n,end)-start +1;
+            totalComma += count * commas;
+            start *= 1000;
+            commas++; 
+        }
+        return totalComma;
     }
 }
 
-// 10000 => 10,000 
+// 1000(999) to 9999 → 9000 numbers → each has 1 comma
+
+// 10000(9999) → 12000 → 2001 numbers → each has 1 comma
+
+// Total commas:
+
+// (9000 × 1) + (2001 × 1) = 11001
