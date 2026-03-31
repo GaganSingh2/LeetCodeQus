455c5a92b01973d0d91a4498a94beb30d5bdc707
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-03-31 10:49:40 +0530

    Time: 0 ms (100%), Space: 43.2 MB (94.81%) - LeetHub

--- 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.java
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.java
@@ -1,8 +1,8 @@
 class Solution {
     public int firstMatchingIndex(String s) {
         if(s.length()==1) return 0;
-        int si = 0, ei = s.length()-1, n = s.length();
-        while(si<ei){
+        int si = 0, n = s.length();
+        while(si<n-1){
             if(s.charAt(si)==s.charAt(n-si-1)){
                 return si;
             }
