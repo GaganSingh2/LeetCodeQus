7fc57f518e922173b48222471b38f1e7e4fe1863
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-04-17 00:30:37 +0530

    Time: 29 ms (5.52%), Space: 46 MB (39.21%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -15,20 +15,63 @@ class Solution {
         // return flag;
 
 
-        // 2nd Approach
+        // 2nd Approach---------------------
 
         
-        if(x<0) return false;
-        int res = 0, temp = x;
-        while(temp>0){
-            int dig = temp % 10;
-            res = res*10+dig;
-            temp /= 10;
-        }
-        if(x == res){
-            return true;
-        }
-        return false;
+
+
+
+
+
+
+
+
+if(x<0){
+    return false;
+}
+int res=x;
+int rev = 0;
+while(x>0){
+    int digit = x%10;
+    int temp=rev*10+digit;
+    rev=temp;
+    x/=10;  
+}
+System.out.println(rev);
+if(res==rev){
+return true;
+}
+return false;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // if(x<0) return false;
+        // int res = 0, temp = x;
+        // while(temp>0){
+        //     int dig = temp % 10;
+        //     res = res*10+dig;
+        //     temp /= 10;
+        // }
+        // if(x == res){
+        //     return true;
+        // }
+        // return false;
         
 
     }
