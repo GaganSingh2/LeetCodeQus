2276fe85f745ce2a83ee8b42571b496a06b2203b
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-05-19 20:02:12 +0530

    Time: 2 ms (88.98%), Space: 80.4 MB (63.48%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,13 +1,34 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        Set<Integer> freq = new HashSet<>();
-        for(int i=0; i<nums1.length; i++){
-            freq.add(nums1[i]);
-        }
+        //1st Approach------------------------
 
-        for(int i=0; i<nums2.length; i++){
-            if(freq.contains(nums2[i])){
-                return nums2[i];
+        // Set<Integer> freq = new HashSet<>();
+        // for(int i=0; i<nums1.length; i++){
+        //     freq.add(nums1[i]);
+        // }
+
+        // for(int i=0; i<nums2.length; i++){
+        //     if(freq.contains(nums2[i])){
+        //         return nums2[i];
+        //     }
+        // }
+        // return -1;
+
+        //2nd Approach-----------------------------------
+        int si1 = 0, si2 = 0;
+        while(si1<nums1.length && si2<nums2.length){
+            if(nums1[si1]==nums2[si2]){
+                return nums1[si1];
+            }
+            else if(nums1[si1]<nums2[si2]){
+                si1++;
+            }
+            else if(nums1[si1]>nums2[si2]){
+                si2++;
+            }
+            else{
+                si1++;
+                si2++;
             }
         }
         return -1;
