41127c6e7fe8e06718e808f745ebd8ef5376e85d
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-05-31 00:32:54 +0530

    Time: 10 ms (9.84%), Space: 47.8 MB (5.2%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,24 +8,65 @@
  * }
  */
 class Solution {
+    //Helper Method to Store the node from rootNode to n in ArrayList
+    private boolean getPath(TreeNode root, TreeNode n, ArrayList<TreeNode> path){
+        if(root == null){
+            return false;
+        }
+        path.add(root);
+        if(root == n){
+            return true;
+        }
+        boolean isLeft = getPath(root.left, n, path);//stroe
+        boolean isRight = getPath(root.right, n, path);
+
+        if(isLeft || isRight){
+            return true;
+        }
+        path.remove(path.size()-1);//means node n is not exist in leftSubTree and RightSubTree
+        return false;
+    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //1st Approach by using Recursion ----------------
+        // if(root == null){
+        //     return null;
+        // }
+        // if(root == p || root == q){
+        //     return root;
+        // }
+
+        // TreeNode leftLCA = lowestCommonAncestor(root.left, p, q);//check in left SubTree
+        // TreeNode rightLCA = lowestCommonAncestor(root.right, p, q);//check in right SubTree
+
+        // if(leftLCA == null){ //rightLCA != null && leftLCA == null, so rightLCA
+        //     return rightLCA;
+        // }
+        // if(rightLCA == null){ //rightLCA == null && leftLCA != null, so leftLCA
+        //     return leftLCA;
+        // }
+
+        // return root; //leftLCA != null && rightLCA != null means root is result
+
+        //2nd Approach by Using Path Based Approach----------------------
         if(root == null){
             return null;
         }
-        if(root == p || root == q){
-            return root;
-        }
+        ArrayList<TreeNode> path1 = new ArrayList<>();
+        ArrayList<TreeNode> path2 = new ArrayList<>();
 
-        TreeNode leftLCA = lowestCommonAncestor(root.left, p, q);//check in left SubTree
-        TreeNode rightLCA = lowestCommonAncestor(root.right, p, q);//check in right SubTree
+        boolean foundLeft = getPath(root, p, path1);//store from root to p nodes
+        boolean foundRight = getPath(root, q, path2);//store from root to q nodes
 
-        if(leftLCA == null){ //rightLCA != null && leftLCA == null, so rightLCA
-            return rightLCA;
+        if(!foundLeft || !foundRight){
+            return null;//means givens n1 or n2 is not present in whole Binary Tree
         }
-        if(rightLCA == null){ //rightLCA == null && leftLCA != null, so leftLCA
-            return leftLCA;
+        int i = 0;
+        while(i<path1.size() && i<path2.size()){
+            if(path1.get(i) != path2.get(i)){
+                break;
+            }
+            i++;
         }
-
-        return root; //leftLCA != null && rightLCA != null means root is result
+        return path1.get(i-1);//means last comman node is LCA
     }
 }
