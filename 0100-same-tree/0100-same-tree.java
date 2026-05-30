b89d57448fcfa0c25762e1743ea35a0cfac7a72d
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-05-30 20:33:20 +0530

    Time: 0 ms (100%), Space: 43.1 MB (14.73%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,7 +14,7 @@
  * }
  */
 class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+    private boolean isSame(TreeNode p, TreeNode q){
         if(p == null && q == null){
             return true;
         }
@@ -24,9 +24,70 @@ class Solution {
         if(p.val != q.val){
             return false;
         }
-        boolean isLeft = isSameTree(p.left, q.left);
-        boolean isRight = isSameTree(p.right, q.right);
+        Queue<TreeNode> q1 = new LinkedList<>();
+        Queue<TreeNode> q2 = new LinkedList<>();
+        q1.add(p);
+        q1.add(null);
+        q2.add(q);
+        q2.add(null);
+
+        while(!q1.isEmpty() && !q2.isEmpty()){
+            TreeNode currNode1 = q1.remove();
+            TreeNode currNode2 = q2.remove();
+            if((currNode1 == null && currNode2 != null) || (currNode1 != null && currNode2 == null)){
+                return false;
+            }
+            if(currNode1 == null && currNode2 == null){
+                if(q1.isEmpty() && q2.isEmpty()){
+                    return true;
+                }
+                else{
+                    q1.add(null);
+                    q2.add(null);
+                }
+            }
+            else{
+                if(currNode1.val != currNode2.val){
+                    return false;
+                }
+                if((currNode1.left == null && currNode2.left != null) ||
+                 (currNode1.left != null && currNode2.left == null)){
+                    return false;
+                }
+                if(currNode1.left != null){
+                    q1.add(currNode1.left);
+                    q2.add(currNode2.left);
+                }
+                if((currNode1.right == null && currNode2.right != null) ||
+                 (currNode1.right != null && currNode2.right == null)){
+                    return false;
+                }
+                if(currNode1.right != null){
+                    q1.add(currNode1.right);
+                    q2.add(currNode2.right);
+                }
+                
+            }
+        }
+        return true;
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        //1st Approach using Recursion------------
+        // if(p == null && q == null){
+        //     return true;
+        // }
+        // if(p == null || q == null){
+        //     return false;
+        // }
+        // if(p.val != q.val){
+        //     return false;
+        // }
+        // boolean isLeft = isSameTree(p.left, q.left);
+        // boolean isRight = isSameTree(p.right, q.right);
         
-        return isLeft && isRight;
+        // return isLeft && isRight;
+
+        //2nd Approach using Level Order Traversal-----------
+        return isSame(p,q);
     }
 }
