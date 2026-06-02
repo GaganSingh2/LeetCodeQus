0514931df05f801241d32ebaff596aebb31b69ed
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-06-02 17:11:14 +0530

    Time: 1 ms (99.51%), Space: 42 MB (96.7%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,10 +1,29 @@
 class Solution {
     public int digitFrequencyScore(int n) {
+        //1st Approach-------------------TC: O(n), SC: O(1)
+        // int totalSum = 0;
+        // while(n>0){
+        //     int digit = n % 10;
+        //     totalSum += digit;
+        //     n /= 10;
+        // }
+        // return totalSum;
+
+        //2nd Approach------------------
         int totalSum = 0;
+        int freq[] = new int[10];
         while(n>0){
-            int digit = n % 10;
-            totalSum += digit;
+            int digit = n%10;
+            freq[digit-0]++;
             n /= 10;
+        }
+        int digit = 0;
+        for(int i=0; i<freq.length; i++){
+            if(freq[i]>0){
+                // System.out.println(freq[i]+" "+freq[i-0])
+                int currSum = freq[i] * i-0;
+                totalSum += currSum;
+            }
         }
         return totalSum;
     }
