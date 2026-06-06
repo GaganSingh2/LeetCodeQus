41f56c476cfa315a67785f6e2693a82c2f730338
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-06-06 23:46:07 +0530

    Time: 1 ms (83.46%), Space: 46.7 MB (28.57%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -14,41 +14,62 @@
  * }
  */
 class Solution {
-    public int findBottomLeftValue(TreeNode root) {
-        if(root == null){
-            return -1;
+    int ans = 0;
+    int l = 0;
+    //Helper method find bottom level value using Recursion (Preorder Traversal)
+    private void findBottomLeftValUsingRecursion(TreeNode root, int level){
+        if(root == null) return;
+
+        findBottomLeftValUsingRecursion(root.left, level + 1);
+        if(root.left == null && root.right == null && level>l){
+            ans = root.val;
+            l = level;
         }
 
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        q.add(null);
-        int leftVal = root.val;
-        int cnt = 1;
-        while(!q.isEmpty()){
-            TreeNode currNode = q.remove();
-            if(currNode == null){
-                if(q.isEmpty()){
-                    return leftVal;
-                }
-                else{
-                    q.add(null);
-                    cnt++;
-                }
-            }
-            else{
-                if(cnt == 1){
-                    leftVal = currNode.val;
-                    cnt = 0;
-                }
-                if(currNode.left != null){
-                    q.add(currNode.left);
-                }
-                if(currNode.right != null){
-                    q.add(currNode.right);
-                }
-            }
-        }
-        return leftVal;
+        findBottomLeftValUsingRecursion(root.right, level + 1);
+    }
+    public int findBottomLeftValue(TreeNode root) {
+        //1st Approach using Recursion (PreOrder Traversal)--------------------
+        ans = root.val;
+        findBottomLeftValUsingRecursion(root, 0);
+        return ans;
+
+        
+        //2nd Approach By Using Level Order Traversal---------------------
+        // if(root == null){
+        //     return -1;
+        // }
+
+        // Queue<TreeNode> q = new LinkedList<>();
+        // q.add(root);
+        // q.add(null);
+        // int leftVal = root.val;
+        // int cnt = 1;
+        // while(!q.isEmpty()){
+        //     TreeNode currNode = q.remove();
+        //     if(currNode == null){
+        //         if(q.isEmpty()){
+        //             return leftVal;
+        //         }
+        //         else{
+        //             q.add(null);
+        //             cnt++;
+        //         }
+        //     }
+        //     else{
+        //         if(cnt == 1){
+        //             leftVal = currNode.val;
+        //             cnt = 0;
+        //         }
+        //         if(currNode.left != null){
+        //             q.add(currNode.left);
+        //         }
+        //         if(currNode.right != null){
+        //             q.add(currNode.right);
+        //         }
+        //     }
+        // }
+        // return leftVal;
     }
 }
 
