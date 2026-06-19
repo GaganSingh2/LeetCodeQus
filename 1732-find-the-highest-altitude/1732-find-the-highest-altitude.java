bab52d5dbba0261283b1de80e49b088dfb2be13d
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-06-19 21:35:08 +0530

    Time: 0 ms (100%), Space: 43.2 MB (37.26%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,17 +1,28 @@
 class Solution {
     public int largestAltitude(int[] gain) {
-        int prefixSum[] = new int[gain.length+1];
-        int sum = 0, idx = 0;
+
+        //TC: O(n), SC: O(n)---------
+        // int prefixSum[] = new int[gain.length+1];
+        // int sum = 0, idx = 0;
     
+        // for(int i=0; i<gain.length; i++){
+        //     prefixSum[idx++] = sum;
+        //     sum += gain[i];
+        // }
+        // prefixSum[idx] = sum;
+        // int highestAlltitued = prefixSum[0];
+        // for(int i=1; i<prefixSum.length; i++){
+        //     // System.out.println(prefixSum[i]+" ");
+        //     highestAlltitued = Math.max(highestAlltitued, prefixSum[i]);
+        // }
+
+        // return highestAlltitued;
+
+        //TC: O(n), SC: O(1)-----------
+        int prefixSum = 0, highestAlltitued = 0;
         for(int i=0; i<gain.length; i++){
-            prefixSum[idx++] = sum;
-            sum += gain[i];
-        }
-        prefixSum[idx] = sum;
-        int highestAlltitued = prefixSum[0];
-        for(int i=1; i<prefixSum.length; i++){
-            // System.out.println(prefixSum[i]+" ");
-            highestAlltitued = Math.max(highestAlltitued, prefixSum[i]);
+            prefixSum += gain[i];
+            highestAlltitued = Math.max(highestAlltitued, prefixSum);
         }
 
         return highestAlltitued;
