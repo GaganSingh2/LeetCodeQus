9e15ee323f56f260d908dbe9a3bac8702e9d3d40
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-06-24 22:49:56 +0530

    Time: 4 ms (90.67%), Space: 82.7 MB (68.87%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,28 +1,30 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        //1st Aapproach using LinkedList slow-fast pointer TC:O(n) & SC:O(1)
+        int slow = nums[0];
+        int fast = nums[0];
+        //find the intersection point
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //2nd Approach using HashSet TC:O(n) & SC:O(n)
-        HashSet<Integer> freq = new HashSet<>();
-        for(int i=0; i<nums.length; i++){
-            if(freq.contains(nums[i])){
-                return nums[i];
-            }
-            freq.add(nums[i]);
+        //after that re-initalize the slow = nums[0] and fast at the last iterated index
+        slow = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+        return slow;
+
+        //2nd Approach using HashSet TC:O(n) & SC:O(n)----------------------
+        // HashSet<Integer> freq = new HashSet<>();
+        // for(int i=0; i<nums.length; i++){
+        //     if(freq.contains(nums[i])){
+        //         return nums[i];
+        //     }
+        //     freq.add(nums[i]);
+        // }
+        // return -1;
     }
 }
