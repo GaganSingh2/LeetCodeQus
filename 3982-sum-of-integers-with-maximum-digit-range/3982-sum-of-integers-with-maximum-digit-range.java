20390026050602b24170e21b5b100b49bd269523
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-07-05 14:30:58 +0530

    Time: 1 ms (100%), Space: 45.3 MB (49.4%) - LeetHub

--- 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.java
+++ 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.java
@@ -1,31 +1,57 @@
 class Solution {
     public int maxDigitRange(int[] nums) {
+        //Approach-1 TC:O(n.d) SC:O(1) where n is length of nums array and d is maximum number of digit in Array
         int totalSum = 0;
-        int maxDiff = 0, cnt = 0;
-        int temp[] = new int[nums.length];
+        int maxDiff = -1;
         for(int i=0; i<nums.length; i++){
-            int currDiff = 0;
+
             int max = Integer.MIN_VALUE;
             int min = Integer.MAX_VALUE;
             int val = nums[i];
-            while(val>0){
+            while(val > 0){
                 int digit = val % 10;
                 max = Math.max(max, digit);
                 min = Math.min(min, digit);
                 val /= 10;
+            } 
+            int currDiff = max - min;
+            if(currDiff > maxDiff){
+                totalSum = nums[i];
+                maxDiff = currDiff;
             }
-            currDiff = max - min;
-            temp[i] = currDiff;
-            maxDiff = Math.max(maxDiff, currDiff);
-        }
-
-        for(int i=0; i<nums.length; i++){
-            
-            if(temp[i]==maxDiff){
+            else if(currDiff == maxDiff){
                 totalSum += nums[i];
-                // System.out.println(temp[i]+" "+maxDiff+" "+totalSum+" h");
             }
         }
         return totalSum;
+
+        //Approach-2 using Array Counter TC:O(n.d) SC:O(n)
+        // int totalSum = 0;
+        // int maxDiff = 0, cnt = 0;
+        // int temp[] = new int[nums.length];
+        // for(int i=0; i<nums.length; i++){
+        //     int currDiff = 0;
+        //     int max = Integer.MIN_VALUE;
+        //     int min = Integer.MAX_VALUE;
+        //     int val = nums[i];
+        //     while(val>0){
+        //         int digit = val % 10;
+        //         max = Math.max(max, digit);
+        //         min = Math.min(min, digit);
+        //         val /= 10;
+        //     }
+        //     currDiff = max - min;
+        //     temp[i] = currDiff;
+        //     maxDiff = Math.max(maxDiff, currDiff);
+        // }
+
+        // for(int i=0; i<nums.length; i++){
+            
+        //     if(temp[i]==maxDiff){
+        //         totalSum += nums[i];
+        //         // System.out.println(temp[i]+" "+maxDiff+" "+totalSum+" h");
+        //     }
+        // }
+        // return totalSum;
     }
 }
