ed5b5e08193b90f94b4d060da58a87cf72336ab3
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-07-21 10:56:17 +0530

    Time: 9 ms (16.06%), Space: 47.5 MB (8.72%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,46 +1,45 @@
 class Solution {
-    private void rotate(int si, int ei, ArrayList<Integer> temp){
+    private void rotate(int si, int ei, ArrayList<Integer> path){
         while(si<ei){
-            int val = temp.get(si);
-            temp.set(si, temp.get(ei));
-            temp.set(ei, val);
+            int temp = path.get(si);
+            path.set(si, path.get(ei));
+            path.set(ei, temp);
+
             si++;
             ei--;
         }
     }
-    private void rotateList(ArrayList<Integer> temp, int k){
-        rotate(0, temp.size()-1, temp);
-        rotate(0, k-1, temp);
-        rotate(k, temp.size()-1, temp);
+    private void rotateList(ArrayList<Integer> path, int k){
+        k %= path.size();
+
+        rotate(0, path.size()-1, path);
+        rotate(0, k-1, path);
+        rotate(k, path.size()-1, path);
     }
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
-        int n = grid[0].length;
-        int m = grid.length;
-        int total = n * m;
-        k %= total;
 
-        ArrayList<Integer> temp = new ArrayList<>();
+        ArrayList<Integer> path = new ArrayList<>();
         for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
-                temp.add(grid[i][j]);
+            for(int j=0; j<grid[i].length; j++){
+                path.add(grid[i][j]);
             }
         }
-        rotateList(temp, k);
+        rotateList(path, k);
         
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> curr = new ArrayList<>();
         int idx = 0;
-        for(int i=0; i<temp.size(); i++){
+        ArrayList<Integer> temp = new ArrayList<>();
+        for(int i=0; i<path.size(); i++){
             if(idx == grid[0].length){
-                res.add(curr);
-                curr = new ArrayList<>();
+                res.add(temp);
                 idx = 0;
+                temp = new ArrayList<>();
             }
+            temp.add(path.get(i));
             idx++;
-            curr.add(temp.get(i));
         }
-        res.add(curr); //last subArray
-        return res;
+        res.add(temp); //last subArray
 
+        return res;
     }
 }
