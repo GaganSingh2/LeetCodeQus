707b6616a55f8d9dc1fb11e74d05924b806e874a
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-07-22 22:36:00 +0530

    Time: 7 ms (73.84%), Space: 49.9 MB (33.93%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,15 +2,22 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> map = new HashMap<>();
         for(int i=0; i<strs.length; i++){
+            //take one-one word at a time
             String str = strs[i];
 
+            //convert it into character type array
             char ch[] = str.toCharArray();
 
+            //sort the character
             Arrays.sort(ch);
 
+            //again convert the sorted character into String
             String key = new String(ch);
+
+            //if this new word is not present in map so create one key with this word with new empty ArrayList
             map.putIfAbsent(key, new ArrayList<>());
 
+            //add the word with respective key
             map.get(key).add(str);
         }
 
