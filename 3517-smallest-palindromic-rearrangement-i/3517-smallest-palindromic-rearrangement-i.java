c70f5eeabae9e7a7f399c3fc1a58eb42a788e016
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-07-28 13:06:30 +0530

    Time: 27 ms (75.1%), Space: 48.3 MB (23.69%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -28,15 +28,12 @@ class Solution {
         StringBuilder res = new StringBuilder(sb);
 
         //then reverse the temp stringbuilder to build second half of the string (bcz in palindrome second half of string is just opposite of the first half of the string)
-        // sb = sb.reverse();
+        sb = sb.reverse();
         
         //if the length is odd type so add middle character of given string into result stringbuilder before the second half string is going to store into the result stringbuilder
         if(s.length() % 2!=0){
             res.append(s.charAt(s.length()/2));
         }
-        for(int i=sb.length()-1; i>=0; i--){
-            res.append(sb.charAt(i));
-        }
-        return res.toString();
+        return res.append(sb).toString();
     }
 }
