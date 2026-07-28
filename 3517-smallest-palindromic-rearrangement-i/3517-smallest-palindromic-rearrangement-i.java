5486597f9790e7acfc54c282107623a3f80814c5
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-07-28 13:00:36 +0530

    Time: 27 ms (75.1%), Space: 48.5 MB (10.04%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -6,11 +6,15 @@ class Solution {
 
         StringBuilder sb = new StringBuilder();
         int freq[] = new int[26];
+
+        //count the frequency of half of the given string s
         for(int i=0; i<s.length()/2; i++){
             // System.out.println(s.charAt(i));
             char ch = s.charAt(i);
             freq[ch-'a']++;
         }
+
+        //append into the characters based on the frequency into the newly StringBuilder
         for(int i=0; i<freq.length; i++){
             if(freq[i]>0){
                 while(freq[i] != 0){
@@ -20,9 +24,13 @@ class Solution {
                 }
             }
         }
+        //store temp stringbuilder into the result stringbuilder
         StringBuilder res = new StringBuilder(sb);
+
+        //then reverse the temp stringbuilder to build second half of the string (bcz in palindrome second half of string is just opposite of the first half of the string)
         sb = sb.reverse();
-        // sb = sb.deleteCharAt(0);
+        
+        //if the length is odd type so add middle character of given string into result stringbuilder before the second half string is going to store into the result stringbuilder
         if(s.length() % 2!=0){
             res.append(s.charAt(s.length()/2));
         }
