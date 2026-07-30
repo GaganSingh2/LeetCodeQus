0af0b4fd3658eb45c7079610564269f2586cb7a0
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-07-30 18:05:17 +0530

    Time: 0 ms (100%), Space: 43.3 MB (28.23%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -18,3 +18,18 @@ class Solution {
         return totalCost;
     }
 }
+/**
+Description and mapping of phone is given only for confusion.
+we simmulate the question like
+Letters 	            Add Cost
+First 8 letters	          1
+Next 8 letters	          2
+Next 8 letters	          3
+Next 8 letters	          4
+
+if we iterate the given String i=0 to len-1
+if i = 0,1,2,3,4,5,6,7         -> cost = 1
+if i = 8,9,10,11,12,13,14,15   -> cost = 2
+if i = 16,17,18,19,20,21,22,23 -> cost = 3
+if i = 24,25                   -> cost = 4
+ */
