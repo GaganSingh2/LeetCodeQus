122317ae91ca76da445b8752c5c27afce394dd70
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-08-03 00:07:56 +0530

    Time: 3 ms (24.2%), Space: 47.1 MB (54.77%) - LeetHub

--- 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.java
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.java
@@ -2,11 +2,13 @@ class Solution {
     public boolean checkXMatrix(int[][] grid) {
         for(int i=0; i<grid.length; i++){
             for(int j=0; j<grid[0].length; j++){
+                //Diagonal Elements of The matrix are non-zero (if zero then return false)
                 if(i==j || (i+j == grid.length-1)){
                     if(grid[i][j] == 0){
                         return false;
                     }
                 }
+                //Non-Diagonal Elements of the matrix are zero (if non-zero then return false)
                 else{
                     if(grid[i][j] != 0){
                         return false;
