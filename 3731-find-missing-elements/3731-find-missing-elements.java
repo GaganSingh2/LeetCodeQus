974466c1969feee61c143e6e989edb3716a4789a
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-08-04 13:43:26 +0530

    Time: 1 ms (100%), Space: 46.8 MB (53.61%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,17 +1,36 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
-        Arrays.sort(nums);
-        int min = nums[0];
-        int max = nums[nums.length-1];
+        //Approach-1 (Sorting & Array) TC: O(nlogn)
+        // Arrays.sort(nums);
+        // int min = nums[0];
+        // int max = nums[nums.length-1];
+
+        // List<Integer> missingEle = new ArrayList<>();
+
+        // for(int i=0; i<nums.length; i++){
+        //     if(nums[i] != min){
+        //         missingEle.add(min);
+        //         i--;
+        //     }
+        //     min++;
+        // }
+        // return missingEle;
+
+        //Approach-2 (HashSet & Array) TC: O(n)
+        boolean seen[] = new boolean[101];
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int ele: nums){
+            min = Math.min(min, ele);
+            max = Math.max(max, ele);
+            seen[ele] = true;
+        }
 
         List<Integer> missingEle = new ArrayList<>();
-
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] != min){
-                missingEle.add(min);
-                i--;
+        for(int i=min; i<=max; i++){
+            if(!seen[i]){
+                missingEle.add(i);
             }
-            min++;
         }
         return missingEle;
     }
