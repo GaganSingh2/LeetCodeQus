20ed0a7af7133bef37b58e195da5c510698dcac5
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-08-14 23:36:00 +0530

    Time: 1 ms (100%), Space: 43.7 MB (59.89%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
@@ -13,22 +13,47 @@ class Solution {
         return true;
     }
     public int maximumLengthSubstring(String s) {
-        if(s.length() == 2){
+
+        //Approach-1 --------TC: O(n), SC:O(1)
+        if(s.length()==2){
             return 2;
         }
+        int freq[] = new int[26]; // s consist only lowercase SC: O(1)
 
-        HashMap<Character, Integer> freq = new HashMap<>();
+        int si = 0;
         int maxLength = 0;
-        for(int i=0; i<=s.length()-2; i++){
-            for(int j=i+1; j<=s.length()-1; j++){
-                String str = s.substring(i,j+1);
-                boolean flag = frequencyCount(str);
-                if(flag){
-                    maxLength = Math.max(maxLength, j-i+1);
+        for(int ei=0; ei<s.length(); ei++){
+            char ch = s.charAt(ei);
+            freq[ch-'a']++;
+            if(freq[ch-'a']>2){
+                while(freq[ch-'a']>2){
+                    char ch2 = s.charAt(si);
+                    freq[ch2-'a']--;
+                    si++;
                 }
             }
+            maxLength = Math.max(maxLength, ei-si+1);
         }
         return maxLength;
+
+
+        //Approach-2----------------- TC:O(n^2), SC: O(n)
+        // if(s.length() == 2){
+        //     return 2;
+        // }
+
+        // HashMap<Character, Integer> freq = new HashMap<>();
+        // int maxLength = 0;
+        // for(int i=0; i<=s.length()-2; i++){
+        //     for(int j=i+1; j<=s.length()-1; j++){
+        //         String str = s.substring(i,j+1);
+        //         boolean flag = frequencyCount(str);
+        //         if(flag){
+        //             maxLength = Math.max(maxLength, j-i+1);
+        //         }
+        //     }
+        // }
+        // return maxLength;
 
     }
 }
