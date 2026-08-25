d91b82a668434d7eb392f199ea9e73cb50c0917a
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-08-25 15:54:39 +0530

    Time: 5 ms (17.87%), Space: 45.1 MB (71.22%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,20 +1,32 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
+        //1st Approach---- TC:O(n log n) SC: O(1)
         int smallestMissingNum = k;
-        HashSet<Integer> flag = new HashSet<>();
+        Arrays.sort(nums);
+        
         for(int i=0; i<nums.length; i++){
-            flag.add(nums[i]);
-        }
-        int cnt = 1;
-        for(int val: flag){
-            if(!flag.contains(smallestMissingNum)){
-                return smallestMissingNum;
-            }
-            else{
-                cnt++;
-                smallestMissingNum = k * cnt;
+            if(nums[i]== smallestMissingNum){
+                smallestMissingNum += k;
             }
         }
         return smallestMissingNum;
+
+        //2nd Approach---- TC: O(n) SC: O(n)
+        // int smallestMissingNum = k;
+        // HashSet<Integer> flag = new HashSet<>();
+        // for(int i=0; i<nums.length; i++){
+        //     flag.add(nums[i]);
+        // }
+        // int cnt = 1;
+        // for(int val: flag){
+        //     if(!flag.contains(smallestMissingNum)){
+        //         return smallestMissingNum;
+        //     }
+        //     else{
+        //         cnt++;
+        //         smallestMissingNum = k * cnt;
+        //     }
+        // }
+        // return smallestMissingNum;
     }
 }
