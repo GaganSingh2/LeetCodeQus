0590a936893f6fb4a41617ebe311f31cff3d424b
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-08-30 13:04:33 +0530

    Time: 2 ms (43.54%), Space: 43.9 MB (40.38%) - LeetHub

--- 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.java
+++ 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.java
@@ -1,20 +1,39 @@
 class Solution {
     public int countSpecialIntegers(int[] nums) {
-        int maxEle = Integer.MIN_VALUE;
-        for(int i=0; i<nums.length; i++){
-            maxEle = Math.max(maxEle, nums[i]);
-        }
+        //1st Approach----- TC: O(n) SC: O(1)
+        // int maxEle = Integer.MIN_VALUE;
+        // for(int i=0; i<nums.length; i++){
+        //     maxEle = Math.max(maxEle, nums[i]);
+        // }
 
-        int freq[] = new int[maxEle+1];
-        freq[nums[0]] = 1;
+        // int freq[] = new int[maxEle+1];
+        // freq[nums[0]] = 1;
+        // for(int i=1; i<nums.length; i++){
+        //     if(nums[i-1] != nums[i]){
+        //         freq[nums[i]]++;
+        //     }
+        // }
+        // int cnt = 0;
+        // for(int i=0; i<freq.length; i++){
+        //     if(freq[i]==1){
+        //         cnt++;
+        //     }
+        // }
+        // return cnt;
+
+
+        //2nd Approach: TC: O(n), SC: O(n)
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(nums[0], 1);
         for(int i=1; i<nums.length; i++){
             if(nums[i-1] != nums[i]){
-                freq[nums[i]]++;
+                map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
             }
         }
+
         int cnt = 0;
-        for(int i=0; i<freq.length; i++){
-            if(freq[i]==1){
+        for(int val: map.keySet()){
+            if(map.get(val) == 1){
                 cnt++;
             }
         }
