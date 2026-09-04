bc2abd31097ebafa5a0829e1ded1ee0092c924cc
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-09-04 11:55:02 +0530

    Time: 1 ms (99.79%), Space: 46.5 MB (15.74%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,25 +1,22 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        int maxVal = Integer.MIN_VALUE;
         int minVal = Integer.MAX_VALUE;
-        int res = -1;
+        int maxVal = Integer.MIN_VALUE;
         for(int i=0; i<nums.length; i++){
             for(int j=0; j<=i; j++){
                 maxVal = Math.max(maxVal, nums[j]);
-                // System.out.println("max"+maxVal);
             }
-            for(int l=i; l<=nums.length-1; l++){
-                minVal = Math.min(minVal,nums[l]);
-                // System.out.println("min"+minVal);
+
+            for(int j=i; j<nums.length; j++){
+                minVal = Math.min(minVal, nums[j]);
             }
-            int currRes = maxVal-minVal;
-            // System.out.println("curr"+currRes);
-            if(currRes<=k){
+
+            int currVal = maxVal - minVal;
+            if(currVal <= k){
                 return i;
-                // System.out.println("res"+res);
             }
-            maxVal = Integer.MIN_VALUE;
             minVal = Integer.MAX_VALUE;
+            maxVal = Integer.MIN_VALUE;
         }
         return -1;
     }
