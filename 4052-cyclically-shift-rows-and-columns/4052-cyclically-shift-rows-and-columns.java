a92abcaa8fb0274143763faedc4b0fd92dbaf6a3
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-09-18 23:30:11 +0530

    Time: 1 ms (100%), Space: 47.4 MB (64.24%) - LeetHub

---
+++ 4052-cyclically-shift-rows-and-columns/4052-cyclically-shift-rows-and-columns.java
@@ -0,0 +1,42 @@
+class Solution {
+    private void rotate(int temp[], int si, int ei){
+        while(si<ei){
+            int curr = temp[si];
+            temp[si] = temp[ei];
+            temp[ei] = curr;
+            si++;
+            ei--;
+        }
+    }
+    public int[][] cyclicShift(int n, int[][] grid, int[] rowShift, int[] colShift) {
+        for(int i=0; i<grid.length; i++){
+            int temp[] = grid[i];
+            
+            int k = rowShift[i] % n;
+            
+            rotate(temp, 0, k-1);
+            rotate(temp, k, temp.length-1);
+            rotate(temp, 0, temp.length-1);
+              
+        }
+
+        for(int j=0; j<n; j++){
+            int k = colShift[j] % n;
+            
+            int temp[] = new int[n];
+            for(int p=0; p<n; p++){
+                temp[p] = grid[p][j];
+            }
+
+            rotate(temp, 0, k-1);
+            rotate(temp, k, temp.length-1);
+            rotate(temp, 0, temp.length-1);
+
+            for(int p=0; p<n; p++){
+                grid[p][j] = temp[p];
+            }    
+        }
+
+        return grid;
+    }
+}
