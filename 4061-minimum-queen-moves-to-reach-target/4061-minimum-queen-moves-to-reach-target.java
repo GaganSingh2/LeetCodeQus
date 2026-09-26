1219cffbd6eb668eac2e77ef1440c9d2af8616f6
Gagan Kumar Singh <153515594+GaganSingh2@users.noreply.github.com>
2026-09-26 23:44:19 +0530

    Time: 1 ms (95.47%), Space: 44.3 MB (26.3%) - LeetHub

---
+++ 4061-minimum-queen-moves-to-reach-target/4061-minimum-queen-moves-to-reach-target.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minQueenMoves(int[] source, int[] target) {
+        if(source[0] == target[0] && source[1] == target[1]){
+            return 0;
+        }
+        if(Math.abs(source[0]-target[0]) == Math.abs(source[1]-target[1])
+          || Math.abs(source[0]-target[0]) == 0 || Math.abs(source[1]-target[1]) == 0){
+            return 1;
+        }
+        return 2;
+    }
+}
